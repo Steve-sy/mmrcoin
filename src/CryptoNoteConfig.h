e84ae2ff00c5858f9e29527516637d3c9070077c
Steve <steve@localhost.localdomain>
2020-04-08 16:12:44 +0300

    fixed some issues

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -145,8 +145,8 @@ const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          //
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1510d13336228debfee9c918ce505efe225d8c94d045fa115";
 
 const std::initializer_list<const char*> SEED_NODES = {
-   "140.82.29.104:15236",
-   "45.77.160.124:15236"
+   "185.127.18.101:15236",
+   "176.88.131.228:15236"
 };
 
 struct CheckpointData {
